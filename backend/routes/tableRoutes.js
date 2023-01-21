c1799f3b038686868bf851f3df3e09704a60a896
TFuray <pk.master881@gmail.com>
2023-01-21 11:54:11 -0800

    server working

--- backend/routes/tableRoutes.js
+++ backend/routes/tableRoutes.js
@@ -1,1 +1,15 @@
-const
+const express = require('express')
+
+const router = express.Router()
+const {
+  getTables,
+  addTable,
+  fireTable,
+  deleteTable
+} = require('../controllers/tableController')
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getTables).post(protect, addTable)
+router.route('/:id').put(protect, fireTable).delete(protect, deleteTable)
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 const cors = require('cors')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5001
-const mainRoutes = require('./routes/main')
 
 connectDB()
 
@@ -11,10 +11,12 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
-app.use('/', mainRoutes)
+app.use('/api/tables', require('./routes/tableRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server started on port ${port}`))
