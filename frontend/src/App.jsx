cf32c20a0d4eaee0d0840e4c2acdd4016bf2b232
TFuray <pk.master881@gmail.com>
2023-01-21 12:49:08 -0800

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 import Table from './components/Table'
+import Header from './components/Header'
 import { useState } from 'react'
 
-function App() {
-  
-
+function App () {
   return (
-    <div className="App">
-      <div className="floorMap">
-      <Table
-        tableNum='101' 
-      />
-      </div>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   )
 }
 
